7f6d6a1be84799b0b620bade5a829e08f0011cc9
femmytedrey <femmytedrey@gmail.com>
2024-01-31 10:32:50 +0100

    styling

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,3 +1,16 @@
 import React, { createContext } from 'react'
+import all_product from '../Components/Assets/all_product'
 
-export default ShopContext = createContext(null)
+export const ShopContext = createContext(null)
+
+const ShopContextProvider = (props)=> {
+    const contextValue = {all_product}
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider
