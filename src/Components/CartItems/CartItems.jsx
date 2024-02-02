e592c53d868fc057c321b76697a4e8040a029643
femmytedrey <femmytedrey@gmail.com>
2024-02-02 23:44:37 +0100

    working on cart page functionalities

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -25,13 +25,39 @@ const CartItems = () => {
                 <p>{e.name}</p>
                 <p>${e.new_price}</p>
                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-                <p>{e.new_price*cartItems[e.id]}</p>
-                <img className='carditems-remove-icon' src={remove_icon} onClick={() => {removeFromCart(e.id)}} alt="remove-icon" />
+                <p>${e.new_price*cartItems[e.id]}</p>
+                <img className='cartitems-remove-icon' src={remove_icon} onClick={() => {removeFromCart(e.id)}} alt="remove-icon" />
             </div>
             <hr />
           </div>
         }
+        return null;
       })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+            <h1>Cart Totals</h1>
+            <div>
+                <div className="cartitems-total-item">
+                    <p>Subtotal</p>
+                    <p>${0}</p>
+                </div>
+                <hr />
+                <div className="cartitems-total-item">
+                    <p>Shipping Fee</p>
+                    <p>Free</p>
+                </div>
+                <div className="cartitems-total-item">
+                    <h3>Total</h3>
+                    <p>${0}</p>
+                </div>
+            </div>
+            <button>PROCEED TO CHECKOUT</button>
+        </div>
+            <div className="cartitems-promocode">
+                <p>If you have a promode, Enter it here</p>
+                <div className="cartitems-promobox"></div>
+            </div>
+      </div>
     </div>
   )
 }
